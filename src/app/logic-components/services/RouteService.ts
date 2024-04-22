4458d47abe221007bbfd5fa8fe2fbb442648786d
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-04-21 20:08:48 -0600

    Update getAllRoutes

--- src/app/logic-components/services/RouteService.ts
+++ src/app/logic-components/services/RouteService.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Route } from "../classes/Route";
+import {BehaviorSubject, Observable, Subject, tap} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoutesService {
+export class RouteService {
   private apiUrl = 'http://localhost:8080/routes';
+  private routesSubject = new BehaviorSubject<Route[]>([]);
+  routes$ = this.routesSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
-
-  public getAllRoutes() {
-    return this.http.get<Array<Route>>(this.apiUrl);
+  constructor(private http: HttpClient) { 
+    this.getAllRoutes();
   }
+
+  // public getAllRoutes() {
+  //   return this.http.get<Array<Route>>(this.apiUrl);
+  // }
+
+  getAllRoutes(): void{
+    this.http.get<Route[]>(this.apiUrl).subscribe(
+      (initialTransporters) => {
+        this.routesSubject.next(initialTransporters);
+      },
+      (error) => {
+        console.error('Error al obtener la lista de transportistas:', error);
+      }
+    );
+  }
+
 
   public saveRoute(route: Route) {
     return this.http.post<Route>(this.apiUrl, route);
