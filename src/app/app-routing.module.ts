449952e8783c6823dcb20a25ceb177be891edddd
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-04-21 18:51:26 -0600

    Add login path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./ui-components/home/home.component";
 import { SignUpComponent } from './ui-components/signup/signup.component';
+import { LoginComponent } from './ui-components/login/login.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {path: 'signup', component: SignUpComponent}
+  {path: 'signup', component: SignUpComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
