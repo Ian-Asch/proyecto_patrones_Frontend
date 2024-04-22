e9093a36a7e32865f19ed6743d8d25c290d8ca9c
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-04-21 20:09:06 -0600

    Add Route Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { MapComponent } from './ui-components/map/map.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './ui-components/home/home.component';
 import { TransporterListComponent } from './ui-components/transporter/transporter-list/transporter-list.component';
+import { RouteListComponent } from './ui-components/route/route-list/route-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import { TransporterFormComponent } from './ui-components/transporter/transporter-form/transporter-form.component';
+import { RouteFormComponent } from './ui-components/route/route-form/route-form.component';
 import {FormsModule} from "@angular/forms";
 import { TransporterEditModalComponent } from './ui-components/transporter/transporter-edit-modal/transporter-edit-modal.component';
+import { RouteEditModalComponent } from './ui-components/route/route-edit-modal/route-edit-modal.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { SignUpComponent } from './ui-components/signup/signup.component';
 import { LoginComponent } from './ui-components/login/login.component';
@@ -25,6 +28,9 @@ import { SuccessResponseModal } from './ui-components/signup/success-response-mo
     TransporterListComponent,
     TransporterFormComponent,
     TransporterEditModalComponent,
+    RouteListComponent,
+    RouteFormComponent,
+    RouteEditModalComponent,
     SignUpComponent,
     LoginComponent,
     ErrorResponseModal,
