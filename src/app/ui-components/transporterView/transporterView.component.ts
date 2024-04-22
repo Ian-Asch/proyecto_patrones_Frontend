0e8eb97145634876f3e2ad9cebcdb76cc5e8eef9
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-04-21 22:19:03 -0600

    Add transporterView

---
+++ src/app/ui-components/transporterView/transporterView.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transporterView',
+  templateUrl: './transporterView.component.html',
+  styleUrls: ['./transporterView.component.css']
+})
+export class TransporterViewComponent {
+  
+}
